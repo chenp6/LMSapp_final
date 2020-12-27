21dc7cf66ec2f0122d90df45ad16108615b1d04f
CJHuang0000 <76636830+CJHuang0000@users.noreply.github.com>
2020-12-27 12:59:12 +0800

    upload Menu

---
+++ StudentMenu.java
@@ -0,0 +1,82 @@
+package ui;
+
+import javax.swing.JPanel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Font;
+import LMSapp.*;
+public class StudentMenu extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public StudentMenu() {
+		Font menuFont = new Font("Btn", Font.BOLD & Font.CENTER_BASELINE, 23);
+		Font menuItemFont = new Font("menuItem", Font.BOLD & Font.CENTER_BASELINE, 18);
+
+		setBounds(0,0,916,47);
+		JMenuBar menuBar = new JMenuBar();
+		add(menuBar);
+		
+		JMenu accountManagement = new JMenu("帳戶管理");
+		accountManagement.setFont(menuFont);
+		menuBar.add(accountManagement);
+		
+		JMenuItem modifyPassword = new JMenuItem("修改密碼");
+		modifyPassword.setFont(menuItemFont);
+		accountManagement.add(modifyPassword);
+		modifyPassword.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				LMSapp.frame.changePage(new ChangePassWordPanel());
+			}
+		});
+		
+		JMenu classInformation = new JMenu("課程資訊");
+		menuBar.add(classInformation);
+		classInformation.setFont(menuFont);
+
+		JMenuItem classSearch = new JMenuItem("課程查詢");
+		classInformation.add(classSearch);
+		classSearch.setFont(menuItemFont);
+		classSearch.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				LMSapp.frame.changePage(new CourseSearchPanel());
+			}
+		});
+
+		JMenuItem myClass = new JMenuItem("我的課程");
+		classInformation.add(myClass);
+		myClass.setFont(menuItemFont);
+		myClass.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				LMSapp.frame.changePage(new MyClassPanel());
+			}
+		});
+
+		JMenu signOut = new JMenu("登出");
+		menuBar.add(signOut);
+		signOut.setFont(menuFont);
+
+		JMenuItem signOutCheck = new JMenuItem("確認登出");
+		signOut.add(signOutCheck);
+		signOutCheck.setFont(menuItemFont);
+		signOutCheck.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				LMSapp.frame.changePage(new LoginPanel());
+				LMSapp.frame.remove(getThisJPanel());
+			}
+		});
+	}
+
+	public JPanel getThisJPanel(){
+		return this;
+	}
+
+}
